67c65933f9f902feac3bf8a1b8e064d89fbcc064
TerryThreatt <terry.threatt@gmail.com>
2020-08-19 18:21:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+
+    function submitData(name, email) {
+
+        let configObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        };
+
+       return fetch('http://localhost:3000/users', configObj)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(obj) {
+                document.body.innerHTML = `
+                <ul>
+                    <li>${obj.id}</li>
+                </ul>`
+            })
+            .catch(function(error) {
+
+                document.body.innerHTML = `
+                <ul>
+                    <li>${error.message}</li>
+                </ul>`
+            })
+
+    }
+
+    submitData('Terry', 'terry.threatt@gmail.com')
