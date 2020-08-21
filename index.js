45559d3014235a0dbf09fdfe797bd6660d6e080d
08fdange <08fdange@gmail.com>
2020-08-21 16:38:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let p = document.createElement('p');
+        p.innerHTML = object.id;
+        document.body.appendChild(p);
+        console.log(object)
+    })
+    .catch(function(error){
+        let p = document.createElement('p');
+        p.innerHTML = error.message;
+        document.body.appendChild(p);
+        console.log(error.message);
+    })
+}
+  
